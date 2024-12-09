aa0ace5bd83c2626f2d8dce5af3c4d88b4678e82
ywh <yuandgz@163.com>
2024-12-09 17:22:58 +0800

    add images

--- src/App.tsx
+++ src/App.tsx
@@ -4,44 +4,50 @@ import { createFiveProvider } from '@realsee/five/react';
 import { ResponsiveFiveCanvas } from './components/ResponsiveFiveCanvas';
 import { FivePluginInit, parseWork } from '@realsee/five'
 import { ToggleFiveModeButton } from './components/ToggleFiveModeButton';
-import { PanoFloorplanRadarPlugin, PanoTagPlugin } from '@realsee/dnalogel';
+import { PanoFloorplanRadarPlugin, PanoTagPlugin,Sculpt } from '@realsee/dnalogel';
+import { OrientationPlugin } from '@realsee/five/plugins'
+
 import PanoFloorplanRadarPanel from './plugins/PanoFloorplanRadarPanel'
 import PanoTagPluginUse from './plugins/PanoTagPluginUse';
 import request from './utils/request'
 import { setToken } from './utils/auth';
+import Use from './plugins/Sculpt/Use'
+
 // five初始化参数请参考
 
 // https://unpkg.com/@realsee/five@latest/docs/interfaces/five.FiveInitArgs.html
 
-// 初始化鉴权
-// function author(){
-//    request('/dev-api/auth/access_token',{
-//     method: 'POST',
-//     isToken:false,
-//     body: JSON.stringify({
-//       app_key:'2708611',
-//       app_secret:'833INYBW1AWY9YINNUYVVBHEJBSPTDII'
-//     }),
-//   }).then(res => {
-//     setToken(res.data.access_token)
-//     getBasicData(res.data.access_token)
-//   })
-// }
+// // 初始化鉴权
+function author(){
+   request('/dev-api/auth/access_token',{
+    method: 'POST',
+    isToken:false,
+    body: JSON.stringify({
+      app_key:'2708611',
+      app_secret:'833INYBW1AWY9YINNUYVVBHEJBSPTDII'
+    }),
+  }).then(res => {
+    setToken(res.data.access_token)
+    getBasicData(res.data.access_token)
+  })
+}
 
-// author()
+author()
 
-// function getBasicData(header){
-//   return request('/dev-api/open/v3/vr/info',{
-//     method: 'GET',
-//     params: {
-//       resource_code:'4oM61gXy4rFY0V6f3D',
-//     },
-//     headers:{"Authorization":header}
-//   }).then(res=> {
-//     console.log(res);
-//   })
-// }
+function getBasicData(header){
+  return request('/dev-api/open/v3/vr/info',{
+    method: 'GET',
+    params: {
+      resource_code:'4oM61gXy4rFY0V6f3D',
+    },
+    headers:{"Authorization":header}
+  }).then(res=> {
+    console.log(res);
+  })
+}
 
+const lineWidth = 1.5
+const lineOpacity = 0.8
 
 const FiveProvider = createFiveProvider({
   imageOptions: {
@@ -55,21 +61,50 @@ const FiveProvider = createFiveProvider({
   },
   plugins: [
     // [PanoFloorplanRadarPlugin, 'panoFloorplanRadarPlugin'],
-    // [PanoTagPlugin, 'panoTagPlugin', { debug: false }] as FivePluginInit<typeof PanoTagPlugin>
+    [PanoTagPlugin, 'panoTagPlugin', { debug: false }] as FivePluginInit<typeof PanoTagPlugin> ,
+    [OrientationPlugin, 'OrientationPlugin'],
+    [
+      (five) =>
+        new Sculpt(five, {
+          point: { color: 0xffffff },
+          line: { lineColor: 0xffffff, lineWidth },
+          polyline: { lineColor: 0x000000, lineWidth },
+          polygon: { color: 0xffffff, lineColor: 0x000000, lineWidth, lineOpacity },
+          prism: { color: 0xffffff, lineColor: 0x000000, lineWidth, lineOpacity },
+          rectangle: { color: 0xffffff, lineColor: 0x000000, lineWidth, lineOpacity },
+          circle: { color: 0xffffff, lineColor: 0x000000, lineWidth, lineOpacity },
+          cylinder: { color: 0xffffff, lineColor: 0x000000, lineWidth, lineOpacity },
+          box: { color: 0xffffff, lineColor: 0x000000, lineWidth, lineOpacity },
+        }),
+      'Sculpt',
+    ],
   ],
+  poweredByRealsee:false
 });
 
 function App() {
   return (
     workJSON && (
-      <FiveProvider initialWork={parseWork(workJSON)}>
+      <FiveProvider 
+        initialWork={parseWork(workJSON)}
+        initialOptions={{
+          '3d-tiles': {
+            minLevelOfDetail: 1,
+            maxScreenSpaceError: 1,
+          },
+        }}
+        initialState={{ mode: 'Mapview', latitude: 0.78, longitude: 2 }}
+        >
         {/* 基础resize */}
-        <ResponsiveFiveCanvas />
-        <ToggleFiveModeButton/>
+        <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
+          <ResponsiveFiveCanvas />
+        </div>
+        {/* <ToggleFiveModeButton/> */}
         {/* 小地图 */}
         {/* <PanoFloorplanRadarPanel/> */}
         {/* 标签tag */}
-        {/* <PanoTagPluginUse/> */}
+        <PanoTagPluginUse/>
+        <Use/>
       </FiveProvider>
     )
   )
--- src/plugins/PanoTagPluginUse.tsx
+++ src/plugins/PanoTagPluginUse.tsx
@@ -1,7 +1,7 @@
 /******* 
  * @Author: yuanweihao
  * @Date: 2024-12-05 16:53:31
- * @LastEditTime: 2024-12-06 15:01:33
+ * @LastEditTime: 2024-12-09 16:16:12
  * @LastEditors: yuanweihao
  * @FilePath: \realsee-template-react\src\plugins\PanoTagPluginUse.tsx
  * @Description: 标签use
@@ -9,7 +9,7 @@
 import { useEffect, useState } from 'react'
 import { unsafe__useFiveInstance, useFiveEventCallback, useFiveState } from '@realsee/five/react'
 import { Five, Mode } from '@realsee/five'
-import { TagsList } from '../mocks/mock_data.ts'
+import { TagsList } from '../mocks/mock_a.ts'
 import TagsList6 from '../mocks/crash/maxTest.ts'
 import TagsList3 from '../mocks/mediaModel.ts'
 import TagsList2 from '../mocks/marketingData.ts'
@@ -20,6 +20,7 @@ import TagsListCrash from '../mocks/crash/tag.json'
 import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 import { PanoTagPluginExportInterface } from '@realsee/dnalogel/dist'
+import {Raycaster,Vector2} from 'three'
 // import { FiveModeSwitcher } from '../components/FiveModeSwitcher.tsx'
 
 const PanoTagPluginUse = () => {
@@ -59,10 +60,43 @@ const PanoTagPluginUse = () => {
     })
   }
 
+  // const raycaster = new Raycaster();
+  // const mouse = new Vector2();
+  // function onMouseClick(event) {
+  //   // 将鼠标点击位置转换为标准化设备坐标 (NDC)
+  //   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  //   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  
+  //   // 更新射线投射器以匹配相机和鼠标位置
+  //   raycaster.setFromCamera(mouse, pluginInstance.five.camera);
+  
+  //   // 计算所有与射线相交的对象
+  //   const intersects = raycaster.intersectObjects(pluginInstance.five.scene.children, true); // true 表示递归查找子对象
+  
+  //   if (intersects.length > 0) {
+  //      // 获取第一个被击中的对象及其点击点的世界坐标
+  //   const hitObject = intersects[0].object;
+  //   const point = intersects[0].point;
+
+  //   // console.log('Hit object:', hitObject);
+  //   console.log('World coordinates of the hit point:', point);
+
+  //   // 如果需要局部坐标，可以这样做：
+  //   // const localPoint = hitObject.worldToLocal(point.clone());
+  //   // console.log('Local coordinates of the hit point:', localPoint);
+
+  //   // // 打印出具体的 x, y, z 坐标值
+  //   // console.log(`World X: ${point.x.toFixed(2)}, Y: ${point.y.toFixed(2)}, Z: ${point.z.toFixed(2)}`);
+  //   // console.log(`Local X: ${localPoint.x.toFixed(2)}, Y: ${localPoint.y.toFixed(2)}, Z: ${localPoint.z.toFixed(2)}`);
+  //   } else {
+  //     console.log('Nothing clicked');
+  //   }
+  // }
+  // window.addEventListener('click', onMouseClick, false);
+  
   useEffect(() => {
     loadData0()
     return () =>{
-      console.log('dispose');
       pluginInstance.dispose()
     } 
   },[five])
